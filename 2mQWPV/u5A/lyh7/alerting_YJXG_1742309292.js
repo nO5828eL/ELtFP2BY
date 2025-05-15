ea687c93a52c63943a0c415a7318aa12d8a67b90
nO5828eL <ships.gulches0r@icloud.com>
2025-05-15 20:12:13 +0100

    Update the file's README with new usage instructions

--- 2mQWPV/u5A/lyh7/alerting_YJXG_1742309292.js
+++ 2mQWPV/u5A/lyh7/alerting_YJXG_1742309292.js
@@ -49,7 +49,6 @@ module.exports = [
   },
   {
     name: 'react/jsx-runtime',
-    // @ts-ignore - not sure why but flat config is typed as a maybe?
     ...reactPlugin.configs.flat['jsx-runtime'],
   },
   {
